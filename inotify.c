54eb536313855cfd71fa83dde818000088bad997
Jean Fernand RAMANANTOANINA <123803523+Dahfer99@users.noreply.github.com>
2026-05-21 08:51:41 +0300

    watch static recursive

--- inotify.c
+++ inotify.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
-#include<signal.h>
+#include <signal.h> //Gère les signals
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
+#include <unistd.h> // gère les I/O system call
+#include <string.h> // gère le chaîne de caractère
+#include <errno.h> // gère les erreur
 #include <limits.h>
+#include <dirent.h> // gère les répértoires
 #include <sys/inotify.h>
+#include <sys/stat.h>
 
 #define EVENT_SIZE sizeof(struct inotify_event)
 #define EVENT_BUF_LEN (10 * (EVENT_SIZE + NAME_MAX + 1))
 
+void watch_recursive(const char *path);
+void cleanup(int sig);
 
+int fd, wd[32],wd_count;
 
-int fd, wd[12],wd_count;
+void watch_recursive(const char *path){
+
+	wd[wd_count] = inotify_add_watch(fd, path, IN_ALL_EVENTS);
+	if (wd[wd_count] < 0) { perror(path); return; }
+	wd_count++;
+
+	DIR * dirp = opendir(path);
+	char full_path[512];
+	if (dirp == NULL){
+		perror("dirp");
+		return;
+	}
+
+	struct dirent *entry;
+	while ((entry=readdir(dirp)) != NULL){
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+		if ((entry->d_type) == DT_DIR){
+			snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+			watch_recursive(full_path);
+			/* wd[wd_count]= inotify_add_watch(fd, full_path, IN_ALL_EVENTS);
+			if (wd[wd_count] < 0) {
+				perror("inotify_add_watch - recursive");
+				return;
+			}
+			wd_count++; */
+		}
+
+	}
+}
 
 void cleanup(int sig){
 	for(int i=0; i<wd_count; i++){
@@ -32,9 +65,7 @@ int main(int argc, char **argv)
 	
 	signal(SIGINT, cleanup);   // Ctrl+C
 	signal(SIGTERM, cleanup);  // kill command
-	
-
-	
+		
 	// Initialisation de inotify --- Initialize inotify
 	fd = inotify_init();
 	if (fd < 0)
@@ -51,12 +82,13 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 	
-	// Assignation des wd --- Getting wd
+	
 	while ((fgets(path, sizeof(path), conf)) != NULL)
 	{
 		if (path[0] == '#' || path[0] == '\0'){continue;}
 		path[strcspn(path, "\n")] = '\0';
-		wd[i] = inotify_add_watch(fd, path, IN_ALL_EVENTS);
+		watch_recursive(path); 
+		/*wd[i] = inotify_add_watch(fd, path, IN_ALL_EVENTS);
 		if (wd[i] < 0)
 		{
 			perror("inotify_add_watch");
@@ -65,7 +97,7 @@ int main(int argc, char **argv)
 			exit(1);
 		}
 		i++;
-		wd_count++;
+		wd_count++; */
 	}
 	
 	while (1)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Must learn
+- advenced file operation (fgets, fputs, fflush, ...)
+- dirent.h header
+- sys/stat.h header
+
+# Ask claude
+Break it one by one:
+
+## Only the static recursive system first
+- the get full_path is inside the parsing(config) system?
+
+## Now the dynamic recursive system
